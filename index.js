75775e313a0405c27c344a1b7ef8844ff119a5ae
badr <badr@vps378993.ovh.net>
2017-05-11 22:44:07 +0200

    modification sur les consumers add connection release

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const mysql = require('mysql');
 const bunnymq = require('bunnymq')({
-  host: 'amqp://nzgmkclf:XjPG1TRprZiDChCgreL36AFqA6MxaUMH@lark.rmq.cloudamqp.com/nzgmkclf'
+    host: 'amqp://nzgmkclf:XjPG1TRprZiDChCgreL36AFqA6MxaUMH@lark.rmq.cloudamqp.com/nzgmkclf',
+    prefetch: 5,
+    requeue: true
 }).consumer;
 
-const pool = mysql.createPool('mysql://ykhatal:BFnRnnxnugEvXVY95jdw6Knf@localhost/speedboufe');
+//const pool = mysql.createPool("mysql://root:,>pJRen5)W33HZ,x^w!MDv)_Xy*cypJa>APL*Cg3}'@localhost/speedbouffe");
+
+
+var pool  = mysql.createPool({
+    host     : 'localhost',
+    user     : "root",
+    password : ",>pJRen5)W33HZ,x^w!MDv)_Xy*cypJa>APL*Cg3}'",
+    database : 'speedbouffe',
+    connectionLimit : 42
+});
+
+
 
 function runQuery(query) {
   return new Promise((resolve, reject) => {
     pool.getConnection((err, conn) => {
-      if (err) return reject(err);
-      conn.query(query, (err, res) => {
+	if (err) {
+	    console.log("ERROR CONNECTION : " + err);
+	    return reject(err);
+	}
+	conn.query(query, (err, res) => {
+	conn.release();
         if (err) return reject(err);
         return resolve(res);
       });
@@ -18,11 +35,10 @@ function runQuery(query) {
 }
 
 bunnymq.consume('sql:query:run', (msg) => {
-  console.log(msg);
   if (msg && typeof msg === 'string') {
     return runQuery(msg)
     .then((res) => {
-      console.log(res);
+      //console.log(res);
       return res;
     })
     .catch((err) => {
@@ -30,8 +46,8 @@ bunnymq.consume('sql:query:run', (msg) => {
       return false;
     });
   } else {
+    console.log("type msg undefined");
     return Promise.resolve(false);
   }
 });
 
-process.stdin.resume();
